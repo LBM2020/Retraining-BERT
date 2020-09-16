fe7808561ac8b72f71fc125f9ae2d4f6b6e03d1f
LBM2020 <52284157+LBM2020@users.noreply.github.com>
2020-09-16 22:44:29 +0800

    Update tf2torch.sh

--- tf2torch.sh
+++ tf2torch.sh
@@ -1,1 +1,6 @@
+CURRENT_DIR='pwd'
 
+python $CURRENT_DIR/tf2torch/tf2torch.py \
+--tf_checkpoint_path=$CURRENT_DIR/pretrain_model_output \
+--bert_config_file=$CURRENT_DIR/tf_model/bert_config.json \
+--pytorch_model_path=$CURRENT_DIR/torch_model/pytorch_model.bin
